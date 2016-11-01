1267f4acf0bd3e3a7be623343d41b7ef9fd24b86
Andy Philips <aphilips@tamu.edu>
2016-11-01 10:42:57 -0500

    push thicker lines, better colors

--- AR-Simulation/server.R
+++ AR-Simulation/server.R
@@ -8,8 +8,7 @@ shinyServer(function(input, output) {
 	output$plot <- renderPlot({
 		makeplot <- function()	{
 			y <- arima.sim(n = input$obs, list(ar = input$phi))
-			dataset <- data.frame(Time = seq(1:input$obs),series = "y",y)
-			ggplot(data=dataset, aes(x=Time,y=y, group = series, colour = series)) + geom_line() + theme_minimal() + scale_color_brewer(palette="Accent") + theme(legend.position="none")
+			plot.ts(y, col = 'blueviolet', lwd=5)
 		}
 		if(input$goButton) {			# if user presses Re-Simulate
 			makeplot()
